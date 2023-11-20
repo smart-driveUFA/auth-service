d0a6919e7e2f48afbe71b9e181eb9ff050222a50
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-11-20 18:14:49 +0500

    #29 add celery and celery beat

---
+++ project/celery.py
@@ -0,0 +1,21 @@
+import os
+from datetime import timedelta
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+app = Celery("proj")
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+app.conf.broker_connection_retry_on_startup = True
+app.conf.beat_schedule = {
+    'update-api-key-status': {
+        'task': 'core.tasks.update_api_key_status',
+        'schedule': timedelta(hours=12),
+    },
+}
