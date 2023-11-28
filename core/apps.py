ece29c5956ae3d3b958417cc8feb88d3de642249
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-11-28 17:32:06 +0500

    #36 ok

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+    def ready(self):
+        from core.signals import send_api_key_email_on_creation
