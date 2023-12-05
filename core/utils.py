c5246138dc0ae9c40509f02cc5006a1281feb588
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-12-06 02:18:40 +0500

    fix

--- core/utils.py
+++ core/utils.py
@@ -5,17 +5,19 @@ from core.models import TPI
 
 
 def mixin_tpi_model(create=False, get=False, kwargs=None):
-    lat_start = kwargs.get("lat_start", None)
-    lon_start = kwargs.get("lon_start", None)
-    lat_end = kwargs.get("lat_end", None)
-    lon_end = kwargs.get("lon_end", None)
-    start = kwargs.get("start", None)
-    end = kwargs.get("end", None)
-    highway = kwargs.get("highway", None)
+    lat_start = kwargs.data.get("lat_start", None)
+    lon_start = kwargs.data.get("lon_start", None)
+    lat_end = kwargs.data.get("lat_end", None)
+    lon_end = kwargs.data.get("lon_end", None)
+    start = kwargs.data.get("start", None)
+    end = kwargs.data.get("end", None)
+    highway = kwargs.data.get("highway", None)
+    user = kwargs.user
     if create:
         if all({lat_start, lon_start, start, end, highway, lat_end, lon_end}):
             try:
                 TPI.objects.create(
+                    user=user,
                     lat_start=lat_start,
                     lon_start=lon_start,
                     lon_end=lon_end,
@@ -33,6 +35,7 @@ def mixin_tpi_model(create=False, get=False, kwargs=None):
         if all({lat_start, lon_start, start, end, highway}):
             try:
                 return TPI.objects.get(
+                    user=user,
                     lat_start=lat_start,
                     lon_start=lon_start,
                     start=start,
