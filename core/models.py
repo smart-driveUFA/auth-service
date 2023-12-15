b9ed3a0dcccd2cc617f3caa369fe4005a5a67d6b
kilmamatov <117253418+kilmamatov@users.noreply.github.com>
2023-12-15 21:32:44 +0500

    _generate_composite_id

--- core/models.py
+++ core/models.py
@@ -124,7 +124,7 @@ class TPI(models.Model):
 
         super().save(*args, **kwargs)
 
-    def generate_composite_id(self):
+    def _generate_composite_id(self):
         composite_id_string = f"{self.user.username}|{self.lat_start}|{self.lon_start}|{self.lat_end}|{self.lon_end}|{self.start}|{self.end}|{self.highway}"
 
         self.composite_id = uuid.uuid5(uuid.NAMESPACE_DNS, composite_id_string)
